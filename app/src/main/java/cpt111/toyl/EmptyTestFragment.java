97ec441dcc278bfc5b708fee94b32f1c093c984c
iamaleaf <pgonzalez603@gmail.com>
2018-07-25 21:12:29 +1000

    Adding missing files

---
+++ app/src/main/java/cpt111/toyl/EmptyTestFragment.java
@@ -0,0 +1,22 @@
+package cpt111.toyl;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EmptyTestFragment extends Fragment {
+
+    public EmptyTestFragment() {
+        //constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_timer_list, container, false);
+    }
+}
