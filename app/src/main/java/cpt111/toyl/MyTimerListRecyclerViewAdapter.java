96252de039e26fdc3996f4dfbe9489e6e15ef6c1
iamaleaf <pgonzalez603@gmail.com>
2018-07-26 21:01:59 +1000

    Basic table/detail view implemented with fragments

---
+++ app/src/main/java/cpt111/toyl/MyTimerListRecyclerViewAdapter.java
@@ -0,0 +1,132 @@
+package cpt111.toyl;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import cpt111.toyl.TimerListFragment.OnListFragmentInteractionListener;
+
+import java.util.ArrayList;
+
+// adapter type is the class created at the bottom of this file
+public class MyTimerListRecyclerViewAdapter extends RecyclerView.Adapter<MyTimerListRecyclerViewAdapter.ViewHolder> {
+
+    // log for debugging
+    private static final String TAG = "RecyclerViewAdapter";
+
+    // test data not using dummy
+    private ArrayList<String> mTimerNames = new ArrayList<>();
+    private ArrayList<String> mTimerLengths = new ArrayList<>();
+    private final OnListFragmentInteractionListener mListener;
+
+    public MyTimerListRecyclerViewAdapter(ArrayList<String> mTimerNames, ArrayList<String> mTimerLengths, OnListFragmentInteractionListener listener) {
+        this.mTimerNames = mTimerNames;
+        this.mTimerLengths = mTimerLengths;
+        this.mListener = listener;
+    }
+//
+//    private final List<DummyItem> mValues;
+//    private final OnListFragmentInteractionListener mListener;
+//
+//    public MyTimerListRecyclerViewAdapter(List<DummyItem> items, OnListFragmentInteractionListener listener) {
+//        mValues = items;
+//        mListener = listener;
+//    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_timerlist, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindHolder: called.");
+
+        holder.timerName.setText(mTimerNames.get(position));
+        holder.timerLength.setText(mTimerLengths.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked on: " + mTimerNames.get(position));
+
+
+                AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                //TODO: Testing with existing fragment but need new one... if it works
+                Fragment myFragment = new TimerViewFragment();
+                Bundle bundle = new Bundle();
+
+                bundle.putString("Name", mTimerNames.get(position));
+
+                myFragment.setArguments(bundle);
+                activity.getSupportFragmentManager().beginTransaction().replace(R.id.container, myFragment).addToBackStack(null).commit();
+
+                // what else happens when something is clicked
+            }
+        });
+
+
+
+//        holder.mItem = mTimerNames.get(position);
+//        holder.mIdView.setText(mValues.get(position).id);
+//        holder.mContentView.setText(mValues.get(position).content);
+//
+//        holder.mView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (null != mListener) {
+//                    // Notify the active callbacks interface (the activity, if the
+//                    // fragment is attached to one) that an item has been selected.
+//                    mListener.onListFragmentInteraction(holder.mItem);
+//                }
+//            }
+//        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTimerNames.size();
+    }
+
+
+    // where we set up the view for the "cell"
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+        TextView timerName;
+        TextView timerLength;
+        RelativeLayout parentLayout;
+
+//        public final View mView;
+//        public final TextView mIdView;
+//        public final TextView mContentView;
+//        public String mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+//            mIdView = (TextView) view.findViewById(R.id.item_number);
+//            mContentView = (TextView) view.findViewById(R.id.content);
+            timerName = view.findViewById(R.id.item_number);
+            timerLength = view.findViewById(R.id.content);
+            parentLayout = view.findViewById(R.id.timer_list_parent);
+        }
+
+//        @Override
+//        public String toString() {
+//            return super.toString() + " '" + mContentView.getText() + "'";
+//        }
+    }
+}
---
+++ app/src/main/java/cpt111/toyl/TimerListFragment.java
@@ -0,0 +1,167 @@
+package cpt111.toyl;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+
+
+import static android.support.v7.widget.RecyclerView.HORIZONTAL;
+import static android.support.v7.widget.RecyclerView.VERTICAL;
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+ * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
+ * interface.
+ */
+public class TimerListFragment extends Fragment {
+
+
+    // test data variables
+    private ArrayList<String> mTimerNames = new ArrayList<>();
+    private ArrayList<String> mTimerLengths = new ArrayList<>();
+
+    // TODO: Customize parameter argument names
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+    private OnListFragmentInteractionListener mListener;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public TimerListFragment() {
+    }
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static TimerListFragment newInstance(int columnCount) {
+        TimerListFragment fragment = new TimerListFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setTimersTest();
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_timerlist_list, container, false);
+        Context context = view.getContext();
+
+        RecyclerView recyclerView = view.findViewById(R.id.timer_list);
+        recyclerView.addItemDecoration(new DividerItemDecoration(context, VERTICAL));
+
+        if (mColumnCount <= 1) {
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+        } else {
+            recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+        }
+        recyclerView.setAdapter(new MyTimerListRecyclerViewAdapter(mTimerNames, mTimerLengths, mListener));
+
+        // Set the adapter
+//        if (view instanceof RecyclerView) {
+//            Context context = view.getContext();
+//            RecyclerView recyclerView = (RecyclerView) view;
+//            // line that divides the items in the list
+//            recyclerView.addItemDecoration(new DividerItemDecoration(context, VERTICAL));
+//            if (mColumnCount <= 1) {
+//                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+//
+//            } else {
+//                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+//            }
+//            recyclerView.setAdapter(new MyTimerListRecyclerViewAdapter(mTimerNames, mTimerLengths, mListener));
+        return view;
+
+        }
+
+
+//
+//        RecyclerView timerListView = view.findViewById(R.id.timer_list);
+//        timerListView.setOnItem
+//
+//
+//        timerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItem
+//        });
+
+
+
+
+    private void setTimersTest() {
+        mTimerNames.add("Test Timer 1");
+        mTimerLengths.add("2:00");
+
+        mTimerNames.add("Morning Workout");
+        mTimerLengths.add("20:00");
+
+        mTimerNames.add("This is a fake timer");
+        mTimerLengths.add("1:00");
+
+        mTimerNames.add("We'll see if it works");
+        mTimerLengths.add("29:00");
+
+        System.out.println("Size is " + mTimerLengths.size());
+    }
+
+
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnListFragmentInteractionListener) {
+//            mListener = (OnListFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnListFragmentInteractionListener");
+//        }
+//    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnListFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onListFragmentInteraction();
+    }
+}
