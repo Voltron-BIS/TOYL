ebc854acc761d59ed1ef56f0a340427ed7a7bfd1
iamaleaf <pgonzalez603@gmail.com>
2018-07-26 21:56:15 +1000

    Cleaned up code

--- app/src/main/java/cpt111/toyl/Timer/TimerAddFragment.java
+++ app/src/main/java/cpt111/toyl/Timer/TimerAddFragment.java
@@ -26,10 +26,6 @@ public class TimerAddFragment extends Fragment {
         View mContainer = inflater.inflate(R.layout.fragment_timer_add, container, false);
         TextView textView = (TextView) mContainer.findViewById(R.id.add_timer_text);
         textView.setText("This is the add timer fragment");
-//
-//        View mContainer = inflater.inflate(R.layout.fragment_timer_view, container, false);
-//        TextView textView = (TextView) mContainer.findViewById(R.id.timer_view_text);
-//        textView.setText(getArguments().getString("Name"));
 
         return mContainer;
 
@@ -41,19 +37,4 @@ public class TimerAddFragment extends Fragment {
         //mListener = null;
     }
 }
-//
-//    @Nullable
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//
-//        View mContainer = inflater.inflate(R.layout.fragment_timer_add, container, false);
-//        TextView textView = (TextView) mContainer.findViewById(R.id.add_timer_text);
-//        textView.setText("This is the add timer fragment");
-//
-//        // Inflate the layout for this fragment
-//        return mContainer;
-//    }
-//
-//}
-//
+
