dc06a4671b4f3b075dc49a336cef5469a4c56097
stevenreidy <s3477655@student.rmit.edu.au>
2018-08-04 21:07:08 +1000

    Test Task info added, day selection refined.

--- app/src/main/java/cpt111/toyl/TimeSchedulerFragment.java
+++ app/src/main/java/cpt111/toyl/TimeSchedulerFragment.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +12,10 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 import static cpt111.toyl.R.layout.fragment_time_scheduler;
+
 
 public class TimeSchedulerFragment extends Fragment {
     private static final String TAG = "TimeSchedulerFragment";
@@ -24,7 +23,31 @@ public class TimeSchedulerFragment extends Fragment {
     private Button mDisplaydate;
     private TextView mDisplayDay;
     private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private TextView mDisplaytask_name;
+    private TextView mDisplaytask_category;
+    private TextView mDisplaydescription;
 
+    //test Task info
+    //-----------------------------------------------
+    private Button mTestbutton1;
+    private Button mTestbutton2;
+    private Button mTestbutton3;
+
+    public class Test_info {
+        public String task_Name;
+        public String task_Category;
+        public String task_Description;
+        Test_info(String task_Name, String task_Category, String task_Description)
+        {
+            this.task_Name = task_Name;
+            this.task_Category = task_Category;
+            this.task_Description = task_Description;
+        }
+    }
+
+
+
+    //-----------------------------------------------
 
     public TimeSchedulerFragment(){
 
@@ -33,6 +56,7 @@ public class TimeSchedulerFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
     }
 
@@ -44,6 +68,21 @@ public class TimeSchedulerFragment extends Fragment {
 
         mDisplaydate = rootView.findViewById(R.id.t_s_datepicker);
         mDisplayDay = rootView.findViewById(R.id.day_name);
+        mDisplaytask_name = rootView.findViewById(R.id.task_name);
+        mDisplaytask_category = rootView.findViewById(R.id.task_category);
+        mDisplaydescription = rootView.findViewById(R.id.task_description);
+        mTestbutton1 = rootView.findViewById(R.id.test1);
+        mTestbutton2 = rootView.findViewById(R.id.test2);
+        mTestbutton3 = rootView.findViewById(R.id.test3);
+
+
+        //test Task info
+        //-----------------------------------------------
+
+
+
+
+
 
         //Get current date and set.
         Calendar cal = Calendar.getInstance();
@@ -51,36 +90,14 @@ public class TimeSchedulerFragment extends Fragment {
         int month = cal.get(Calendar.MONTH);
         int day = cal.get(Calendar.DAY_OF_MONTH);
         int day_of_week = cal.get(Calendar.DAY_OF_WEEK);
-        String day_name = "";
-
+        String day_name = Day(day_of_week);
         month = month + 1;
         String todays_date = day + "/" + month + "/" + year;
 
-        switch (day_of_week) {
-            case 1:
-                day_name = "Sunday";
-                break;
-            case 2:
-                day_name = "Monday";
-                break;
-            case 3:
-                day_name = "Tuesday";
-                break;
-            case 4:
-                day_name = "Wednesday";
-                break;
-            case 5:
-                day_name = "Thursday";
-                break;
-            case 6:
-                day_name = "Friday";
-                break;
-            case 7:
-                day_name = "Saturday";
-                break;
-        }
+
         mDisplaydate.setText(todays_date);
         mDisplayDay.setText(day_name);
+
 
 
         mDisplaydate.setOnClickListener(new View.OnClickListener() {
@@ -110,31 +127,7 @@ public class TimeSchedulerFragment extends Fragment {
                 GregorianCalendar GregorianCalendar = new GregorianCalendar(year, month, day);
                 int day_of_week = GregorianCalendar.get(GregorianCalendar.DAY_OF_WEEK);
 
-                String day_name = "";
-
-                switch (day_of_week) {
-                    case 1:
-                        day_name = "Sunday";
-                        break;
-                    case 2:
-                        day_name = "Monday";
-                        break;
-                    case 3:
-                        day_name = "Tuesday";
-                        break;
-                    case 4:
-                        day_name = "Wednesday";
-                        break;
-                    case 5:
-                        day_name = "Thursday";
-                        break;
-                    case 6:
-                        day_name = "Friday";
-                        break;
-                    case 7:
-                        day_name = "Saturday";
-                        break;
-                }
+                String day_name = Day(day_of_week);
 
                 month = month + 1;
                 // TODO: Use date string to retrieve tasks of the date selected?.
@@ -146,9 +139,88 @@ public class TimeSchedulerFragment extends Fragment {
 
             }
         };
+
+        //test Task info buttons
+        mTestbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Test_info task = Get_Test_Info(0);
+                mDisplaytask_name.setText(task.task_Name);
+                mDisplaytask_category.setText(task.task_Category);
+                mDisplaydescription.setText(task.task_Description);
+            }
+        });
+
+        mTestbutton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Test_info task = Get_Test_Info(1);
+                mDisplaytask_name.setText(task.task_Name);
+                mDisplaytask_category.setText(task.task_Category);
+                mDisplaydescription.setText(task.task_Description);
+            }
+        });
+
+        mTestbutton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Test_info task = Get_Test_Info(2);
+                mDisplaytask_name.setText(task.task_Name);
+                mDisplaytask_category.setText(task.task_Category);
+                mDisplaydescription.setText(task.task_Description);
+            }
+        });
+
+
+
         return rootView;
 
     }
 
+    private String Day(int day){
+        String day_name = "";
+        switch (day) {
+            case 1:
+                day_name = "Sunday";
+                break;
+            case 2:
+                day_name = "Monday";
+                break;
+            case 3:
+                day_name = "Tuesday";
+                break;
+            case 4:
+                day_name = "Wednesday";
+                break;
+            case 5:
+                day_name = "Thursday";
+                break;
+            case 6:
+                day_name = "Friday";
+                break;
+            case 7:
+                day_name = "Saturday";
+                break;
+        }
+        return day_name;
+    }
+
+    //test Task info
+    //-----------------------------------------------
+
+    private Test_info Get_Test_Info(int Task){
+        final Test_info[] test;
+        test = new Test_info[3];
+
+        test[0] = new Test_info("Test task 1","tests", "Do a backflip ");
+        test[1] = new Test_info("Test task 2","tests", "Do a frontflip ");
+        test[2] = new Test_info("Test task 3","tests", "Do a sideflip ");
+
+
+        return test[Task];
+    }
+    //-----------------------------------------------
+
 }
+
 
