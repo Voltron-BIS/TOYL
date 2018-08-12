5c9974c09d5bf8634b97090adb8c19539f5c76bd
Dallas Hall <s3461243@student.rmit.edu.au>
2018-08-12 13:57:38 +1000

    Added the SimplerTimer database access object. Which covers all the SQL queries.

---
+++ app/src/main/java/cpt111/toyl/database/database_access_objects/SimpleTimerDOA.java
@@ -0,0 +1,24 @@
+package cpt111.toyl.database.database_access_objects;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import java.util.List;
+import cpt111.toyl.database.entities.SimplerTimerEntity;
+
+/*
+This interface specifies the SQL queries and associates them to methods.
+ */
+@Dao
+public interface SimpleTimerDOA
+{
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
+	void insert(SimplerTimerEntity simplerTimerEntity);
+	
+	@Query("DELETE FROM simple_timer")
+	void deleteAll();
+	
+	@Query("SELECT * FROM simple_timer ORDER BY timer_id ASC")
+	List<SimplerTimerEntity> getAllTimers();
+}
